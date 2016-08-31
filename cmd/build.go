fdb021e149e467b661b67613b14b59a899b3e78e
Paul Crofts <pmcrofts@margic.com>
2016-08-31 07:24:22 -0700

    Manual build

---
+++ cmd/build.go
@@ -0,0 +1,49 @@
+// Copyright © 2016 NAME HERE <EMAIL ADDRESS>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
+
+import (
+	log "github.com/Sirupsen/logrus"
+	"github.com/spf13/cobra"
+)
+
+// buildCmd represents the build command
+var buildCmd = &cobra.Command{
+	Use:   "build",
+	Short: "Execute go build on a package",
+	Long: `Test executes test on a go package
+		First gets the package then executes.
+		Then executs go tests on the package.
+		Writes results to mount`,
+	Run: func(cmd *cobra.Command, args []string) {
+
+		log.Debug("build called")
+	},
+}
+
+func init() {
+	RootCmd.AddCommand(buildCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// buildCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// buildCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+
+}
